3a814e43764403e7effb8defad4f85beda391295
@manuelduarte077 <carlosd99m@gmail.com>
2022-04-24 14:47:38 -0600

    add onboarding

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -3,7 +3,8 @@
 //  Restart
 //
 //  Created by Manuel Duarte on 23/4/22.
-//
+//  https://github.com/manuelduarte077/
+
 
 import SwiftUI
 
@@ -15,17 +16,43 @@ struct OnboaardingView: View {
   //MARK: - BODY
   
     var body: some View {
-      VStack (spacing: 20){
-        Text("Onboarding")
-          .font(.largeTitle) 
-      
-        Button(action: {
-          // Some Action
-          isOnboardingViewActive = false
-        }) {
-          Text("Start")
-        }
-      }
+      ZStack {
+        Color("ColorBlue")
+          .ignoresSafeArea(.all, edges: .all)
+        
+        VStack (spacing: 20){
+          
+          // MARK: - Header
+          
+          Spacer()
+          
+          VStack(spacing: 0){
+                
+            Text("Share.")
+              .font(.system(size: 60))
+              .fontWeight(.heavy)
+              .foregroundColor(.white)
+            
+            Text("""
+              It's not how much we give but
+              how much love we put into giving.
+              """)
+            .font(.title3)
+            .fontWeight(.light)
+            .foregroundColor(.white)
+            .multilineTextAlignment(.center)
+            .padding(.horizontal, 10)
+            
+          } //: HEADER
+          
+          // MARK: - Center
+          
+          
+          // MARK: - Footer
+          
+          
+        } //: VSTACK
+      } //: ZSTACK
     }
 }
 
